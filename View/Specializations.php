774c1763db12c262f9fefe145e3ae2fbdb355b5c
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-05-10 21:24:21 +0600

    navigation bar

--- View/Specializations.php
+++ View/Specializations.php
@@ -6,6 +6,13 @@
     <title>Doctor Specializations</title>
 </head>
 <body>
-    
+    <nav class="navbar">
+        <a href="AdminPanel.php" class = "navbar-logo">CareCraft</a>
+        <ul>
+            <li><a href="AdminPanel.php">Users</a></li>
+            <li><a href="Specializations.php">Specialization</a></li>
+            <li><a href="AdminDoctorDashboard.php">Doctors</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
