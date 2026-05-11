a248af5565b92c6e38bbd90894e7b92d944b946e
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-11 17:43:52 +0600

    first commit

---
+++ View/BrowseDoctors.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Browse Doctors</title>
+    </head>
+    <body>
+        <div class="topbar">
+            <a href="BrowseDoctors.php">Browse Doctors</a>
+            <a href="MyAppointment.php">My Appointment</a>
+</div> 
+
+
+
+
+</body>
+</html>
