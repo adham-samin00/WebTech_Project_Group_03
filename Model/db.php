be1cd4283af56faf65b84905f39278bc8774a153
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-05-11 09:21:58 +0600

    db.php updated

--- Model/db.php
+++ Model/db.php
@@ -6,7 +6,11 @@
             $db_user = "root";
             $db_password = "";
             db_name="hospital_db";
-            
+            $coonnection = new mysqli($db_host,$db_user,$db_password,$db_name);
+            if($connection->connect_error){
+                die("Could not Connect Database".$connection->connect_error);
+            }
+            return $connection;
         }
     }
 ?>
