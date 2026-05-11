9ed5d2758911ce9ec4079cb92bef620370616335
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-05-11 16:52:45 +0600

    adding navigation bar

--- View/AdminDoctors.php
+++ View/AdminDoctors.php
@@ -8,7 +8,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctors</title>
 </head>
-
+    <nav class="navbar">
+        <a href="AdminPanel.php" class = "navbar-logo">CareCraft</a>
+        <ul>
+            <li><a href="AdminPanel.php">Users</a></li>
+            <li><a href="Specializations.php">Specialization</a></li>
+            <li><a href="AdminDoctorDashboard.php">Doctors</a></li>
+        </ul>
+        <a href="../Controller/Logout.php" class = "logout-btn">Logout</a>
+    </nav>
 <body>
     
 </body>
