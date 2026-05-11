baa8f99b92a7b1f977d92f5ba278ac31946cdc37
adham-samin00 <adhamsamin0@gmail.com>
2026-05-11 11:32:56 +0600

    view folder error fixed

--- View/Registration.php
+++ View/Registration.php
@@ -1,6 +1,9 @@
+<?php
+    include "../Controller/RegistrationController.php";
+?>
+
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
