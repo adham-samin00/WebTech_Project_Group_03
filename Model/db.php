261269973dfa0e680ac12b22c6515346af729770
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-05-11 09:18:29 +0600

    db variable

---
+++ Model/db.php
@@ -0,0 +1,12 @@
+<?php
+    class db{
+        function connection()
+        {
+            $db_host = "localhost";
+            $db_user = "root";
+            $db_password = "";
+            db_name="hospital_db";
+            
+        }
+    }
+?>
