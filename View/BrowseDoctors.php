4995587a1edf00c82385bed767e0ac8b21249eb8
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-11 18:29:58 +0600

    level for speialization and fee

--- View/BrowseDoctors.php
+++ View/BrowseDoctors.php
@@ -12,21 +12,27 @@
     <h1>Find a Doctor</h1>
 
 </div>
-<div class="filter-bar">
-        <label for="specialization_id">Select Specialization: </label>
+ <div class="filter-bar">
+        <label for="specialization_id">Search Specialization: </label>
         <select id="specialization_id" name="specialization_id" onchange="FilterDoctors()">
-        <option value="">All Specializations</option>
-        <?php foreach ($specializations as $spec) { ?>
-         <option value="<?php echo $spec["id"] ?>">
-          <?php echo $spec["name"] ?>
-          </option>
-        <?php } ?>
+            <option value="">All Specializations</option>
+            <?php 
+            foreach ($specializations as $spec) {
+             ?>
+                <option value="<?php echo $spec["id"] ?>">
+                    <?php
+                     echo $spec["name"]
+                      ?>
+                </option>
+            <?php } 
+            ?>
         </select>
     </div>
-
-
-
-
+            <p><strong>Specialization:</strong></p>
+            <p><strong>Fee:</strong> </p>
+          
+           <a href="DoctorProfile.php" class="book-btn">View &amp; Book</a>
+    </div>
 
 </body>
 </html>
