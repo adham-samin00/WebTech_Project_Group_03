f77d17b6fe070d994ce930919e1b9171b8ca6de7
Topu369 <topuroy311@gmail.com>
2026-05-12 00:55:22 +0600

    Add filter functionality for managing patient appointments in admin pane

--- View/AdminAppointmentList.php
+++ View/AdminAppointmentList.php
@@ -14,7 +14,27 @@
     </div>
 </div>
 <div class="container">
-    
+    <h1>All Appointments</h1>
+    <p class="subtitle">Filter and manage all patient appointments</p>
+
+    <form method="get" action="AdminAppointmentList.php">
+        <div class="filter-bar">
+            <div>
+                <label for="doctor_id">Doctor</label>
+                <select name="doctor_id" id="doctor_id">
+                    <option value="">All Doctors</option>
+                    <?php foreach ($all_doctors as $doc) { ?>
+                        <option value="<?php echo $doc["id"] ?>"
+                            <?php if ($filter_doctor == $doc["id"]) { echo "selected"; } ?>>
+                            <?php echo $doc["name"] ?>
+                        </option>
+                    <?php } ?>
+
+                </select>
+            </div>
+        </div>
+    </form>
+
 </div>
 </body>
 </html>
