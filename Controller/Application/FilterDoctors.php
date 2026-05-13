e63f94afd45ee15842294c133842edede9efce99
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 19:52:44 +0600

    filter doctor done

--- Controller/Application/FilterDoctors.php
+++ Controller/Application/FilterDoctors.php
@@ -27,3 +27,32 @@ if ($result->num_rows == 0)
         echo "<p class='no-doctors'>No doctors found for this specialization.</p>";
         exit();
     }
+    while ($row = $result->fetch_assoc())
+    {
+        $initial = strtoupper(substr($row["name"], 0, 1));
+        $fee     = number_format($row["consultation_fee"], 0);
+        $spec    = htmlspecialchars($row["specialization"] ?? "General");
+        $name    = htmlspecialchars($row["name"]);
+        $id      = $row["id"];
+
+        if (!empty($row["photo_path"]))
+            {
+                $photo = "<img src='" . htmlspecialchars($row["photo_path"]) . "' class='doc-photo' alt=''>";
+            }
+        else
+            {
+                $photo = "<div class='doc-initial'>$initial</div>";
+            }
+
+        echo "
+        <div class='doc-card'>
+            <div class='doc-card-top'>$photo</div>
+            <div class='doc-card-info'>
+                <h3>$name</h3>
+                <p class='doc-spec'>$spec</p>
+                <p class='doc-fee'>BDT $fee</p>
+                <a href='DoctorProfile.php?id=$id' class='view-btn'>View &amp; Book</a>
+            </div>
+        </div>";
+    }
+?>
