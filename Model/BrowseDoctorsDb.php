c79066a0a305e96b3865fa11a1067bff773bd25f
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 13:12:10 +0600

    spec query added

--- Model/BrowseDoctorsDb.php
+++ Model/BrowseDoctorsDb.php
@@ -1,5 +1,12 @@
 <?php
 include "db.php"
 
+function getAllSpecializations($connection)
+    {
+        $sql    = "SELECT * FROM specializations ORDER BY name ASC";
+        $result = $connection->query($sql);
+        return $result;
+    }
+
 
 ?>
