2ab5d823ebb800202b6c90ef5e0821b21c6e208f
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 13:07:48 +0600

    session created

--- Controller/BrowseDoctorsController.php
+++ Controller/BrowseDoctorsController.php
@@ -1,3 +1,12 @@
 <?php
 include "../Model/BrowseDoctorsdb.php";
+// session_start();
+
+// $isLoggedIn = $_SESSION["loggedIn"] ?? false;
+
+// if (!$isLoggedIn || $_SESSION["role"] != "patient")
+//     {
+//         Header("Location: Login.php");
+//         exit();
+//     }
 ?>
