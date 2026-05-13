050013f2b441da8bbdc37bedb6885fa1ea524156
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 13:49:20 +0600

    session created

---
+++ Controller/DoctorProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../Model/DoctorProfileDb.php";
+// session_start();
+
+// $isLoggedIn = $_SESSION["loggedIn"] ?? false;
+
+// if (!$isLoggedIn || $_SESSION["role"] != "patient")
+//     {
+//         Header("Location: Login.php");
+//         exit();
+//     }
+
+// $doctor_id = intval($_GET["id"] ?? 0);
+
+// if (!$doctor_id)
+//     {
+//         Header("Location: BrowseDoctors.php");
+//         exit();
+//     }
+?>
