a812caec7bc3de4578b6b733e4f0fda5d4c8fe09
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 13:15:16 +0600

    all doctor query added

--- Model/BrowseDoctorsDb.php
+++ Model/BrowseDoctorsDb.php
@@ -8,5 +8,10 @@ function getAllSpecializations($connection)
         return $result;
     }
 
-
+ function getAllActiveDoctors($connection)
+    {
+        $sql    = "SELECT d.id, u.name, s.name AS specialization, d.consultation_fee, d.photo_path, d.specialization_id  FROM doctors d JOIN users u ON d.user_id = u.id LEFT JOIN specializations s ON d.specialization_id = s.id WHERE u.is_active = 1 ORDER BY u.name ASC";
+        $result = $connection->query($sql);
+        return $result;
+    }
 ?>
