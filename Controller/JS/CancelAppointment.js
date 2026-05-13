5a865ba17407e5b37b6c0770809808e1dceebc8e
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 19:09:48 +0600

    josn response

--- Controller/JS/CancelAppointment.js
+++ Controller/JS/CancelAppointment.js
@@ -3,3 +3,8 @@ function CancelAppointment(appointment_id, btn) {
         {
             return;
         }
+         let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(this.responseText);
