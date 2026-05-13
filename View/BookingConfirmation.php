d4acbf3e00910d54b468e9582d304661e5fb4cae
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 14:12:06 +0600

    topnav added

---
+++ View/BookingConfirmation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Booking Confirmed - MediBook</title>
+    <link rel="stylesheet" href="style.css"> -->
+</head>
+<body>
+    <div class="topnav">
+    <a href="BrowseDoctors.php" class="sitename"><span>Medi</span>Book</a>
+    <ul class="nav-links">
+        <li><a href="BrowseDoctors.php">Find a Doctor</a></li>
+        <li><a href="MyAppointments.php">My Appointments</a></li>
+    </ul>
+    <div class="nav-right">
+        <p><?php echo htmlspecialchars($_SESSION["name"]); ?></p>
+        <a href="../Controller/Logout.php" class="logout-btn">Logout</a>
+    </div>
+</div>
+</html>
