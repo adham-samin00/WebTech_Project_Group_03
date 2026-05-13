b809277c9ef6c2e62771fb75b3b10d3f22151fb5
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 19:51:53 +0600

    find doctor

--- Controller/Application/FilterDoctors.php
+++ Controller/Application/FilterDoctors.php
@@ -10,3 +10,20 @@ if (!$isLoggedIn || $_SESSION["role"] != "patient")
         echo "<p>Unauthorized</p>";
         exit();
     }
+     
+    $specialization_id = intval($_GET["specialization_id"] ?? 0);
+
+if ($specialization_id > 0)
+    {
+        $result = getDoctorsBySpecialization( $specialization_id);
+    }
+else
+    {
+        $result = getAllActiveDoctors();
+    }
+
+if ($result->num_rows == 0)
+    {
+        echo "<p class='no-doctors'>No doctors found for this specialization.</p>";
+        exit();
+    }
