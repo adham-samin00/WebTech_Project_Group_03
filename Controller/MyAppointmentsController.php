30f94408176fdc7341ef41416083902ab943eae8
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 15:45:10 +0600

    session created

---
+++ Controller/MyAppointmentsController.php
@@ -0,0 +1,14 @@
+<?php
+
+include "db.php";
+// session_start();
+
+// $isLoggedIn = $_SESSION["loggedIn"] ?? false;
+
+// if (!$isLoggedIn || $_SESSION["role"] != "patient")
+//     {
+//         Header("Location: Login.php");
+//         exit();
+    // }
+     
+     ?>
