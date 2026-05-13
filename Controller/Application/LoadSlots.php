2179e079ccf66093c24f5dc5e115342607c14193
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 19:20:15 +0600

    all slots for every 30  minutes

--- Controller/Application/LoadSlots.php
+++ Controller/Application/LoadSlots.php
@@ -18,3 +18,15 @@ if (!$doctor_id || empty($date))
         echo "<p class='no-slots'>Invalid request.</p>";
         exit();
     }
+    $database   = new db();
+$connection = $database->connection();
+
+// all time slots 09:00 to 17:00 every 30 minutes
+$all_slots = [];
+$start     = strtotime("09:00");
+$end       = strtotime("17:00");
+
+for ($t = $start; $t < $end; $t += 1800)
+    {
+        $all_slots[] = date("H:i", $t);
+    }
