5ec4e583fbc52c8a438bd03068a040bfa19e096e
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 13:56:10 +0600

    database created

---
+++ Model/DoctorProfileDb.php
@@ -0,0 +1,13 @@
+<?php
+include "db.php";
+
+function getDoctorById($connection, $doctor_id)
+    {
+        $sql = "SELECT d.id, d.bio, d.consultation_fee, d.photo_path, d.available_days, u.name, s.name AS specialization FROM doctors d JOIN users u ON d.user_id = u.id LEFT JOIN specializations s ON d.specialization_id = s.id WHERE d.id = ? AND u.is_active = 1";
+        $statement = $connection->prepare($sql);
+        $statement->bind_param("i", $doctor_id);
+        $statement->execute();
+        $result = $statement->get_result();
+        return $result;
+    }
+    ?>
