49bd796bf53c4446d7e1598c74361441ed3bb3c4
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 15:33:43 +0600

    my appointment file created

---
+++ View/MyAppointments.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Appointments - MediBook</title>
+    <!-- <link rel="stylesheet" href="style.css">
+    <script src="../Controller/JS/CancelAppointment.js"></script> -->
+</head>
+<body>
+</html>
