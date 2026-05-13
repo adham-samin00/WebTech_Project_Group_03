e7a79b26965d110b611913cb6825526c1a75fbf6
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 12:36:19 +0600

    nav add

--- View/BrowseDoctors.php
+++ View/BrowseDoctors.php
@@ -1,8 +1,24 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name ="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Find A Doctor Medicare</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find a Doctor - MediBook</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="../Controller/JS/FilterDoctors.js"></script>
 </head>
+<body>
+
+<div class="topnav">
+    <a href="BrowseDoctors.php" class="sitename"><span>Medi</span>Book</a>
+    <ul class="nav-links">
+        <li><a href="BrowseDoctors.php">Find a Doctor</a></li>
+        <li><a href="MyAppointments.php">My Appointments</a></li>
+    </ul>
+    <div class="nav-right">
+        <p><?php echo htmlspecialchars($_SESSION["name"]); ?></p>
+        <a href="../Controller/Logout.php" class="logout-btn">Logout</a>
+    </div>
+</div>
+</body>
 </html>
