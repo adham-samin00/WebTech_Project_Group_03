071eff8db31fc1bdc37e0d490fcda705564526c4
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 15:08:06 +0600

    if error form show again

--- Controller/BookingController.php
+++ Controller/BookingController.php
@@ -61,3 +61,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
                     }
             }
     }
+          $doctor = null;
+          if ($doctor_id)
+    {
+        $result = $database->getDoctorById($connection, $doctor_id);
+        $doctor = $result->fetch_assoc();
+    }
+?>
