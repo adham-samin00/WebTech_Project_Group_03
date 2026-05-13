4e914827f4835dfe92f0d56fb8aaad959f2514ee
adham-samin00 <adhamsamin0@gmail.com>
2026-05-13 12:38:48 +0600

    login controller role fixed

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -45,16 +45,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                                         Header("Location: ../View/PatientHome.php");
                                         exit();
                                     }
-                            elseif ($row["role"] == "doctor")
-                                {
-                                    Header("Location: ../View/DoctorDashboard.php");
-                                    exit();
-                                }
-                            elseif ($row["role"] == "admin")
-                                {
-                                    Header("Location: ../View/AdminPanel.php");
-                                    exit();
-                                }
+                            // elseif ($row["role"] == "doctor")
+                            //     {
+                            //         Header("Location: ../View/DoctorDashboard.php");
+                            //         exit();
+                            //     }
+                            // elseif ($row["role"] == "admin")
+                            //     {
+                            //         Header("Location: ../View/AdminPanel.php");
+                            //         exit();
+                            //     }
                         }
                     else
                         {
