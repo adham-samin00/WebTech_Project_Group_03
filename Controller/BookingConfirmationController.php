28233012c9e7301def547ab36fab5fd087d83fdd
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 15:23:31 +0600

    session created

---
+++ Controller/BookingConfirmationController.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../Model/db.php";
+// session_start();
+
+// $isLoggedIn = $_SESSION["loggedIn"] ?? false;
+
+// if (!$isLoggedIn || $_SESSION["role"] != "patient")
+//     {
+//         Header("Location: Login.php");
+//         exit();
+//     }
+
+// $appointment_id = intval($_GET["id"] ?? 0);
+
+// if (!$appointment_id)
+//     {
+//         Header("Location: BrowseDoctors.php");
+//         exit();
+//     }
+?>
