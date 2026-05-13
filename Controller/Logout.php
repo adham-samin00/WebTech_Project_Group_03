461a35683e569c4e43555ec288e36e2c0d05dbe8
adham-samin00 <adhamsamin0@gmail.com>
2026-05-13 11:01:27 +0600

    Logout updated

--- Controller/Logout.php
+++ Controller/Logout.php
@@ -2,7 +2,7 @@
 
 session_start();
 session_destroy();
-setcookie("user_email", "", time()-3600, "/"); 
+// setcookie("user_email", "", time()-3600, "/"); 
 Header("Location: ../View/Login.php");
 exit();
 ?>
