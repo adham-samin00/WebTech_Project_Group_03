3b6e43c871d5e7e25ded87c3e733dd9c93a7dd47
adham-samin00 <adhamsamin0@gmail.com>
2026-05-13 12:44:41 +0600

    doctor role added

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -45,6 +45,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                                         Header("Location: ../View/PatientHome.php");
                                         exit();
                                     }
+                            elseif ($row["role"] == "doctor")
+                                {
+                                    Header("Location: ../View/DoctorDashboard.php");
+                                    exit();
+                                }
                             elseif ($row["role"] == "admin")
                                 {
                                     Header("Location: ../View/AdminPanel.php");
