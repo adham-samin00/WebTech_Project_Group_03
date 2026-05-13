8b6ee584a73e2dca91ac017f8b6241fd4242bead
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-13 19:06:26 +0600

    filter doctors

---
+++ Controller/JS/FilterDoctors.js
@@ -0,0 +1,14 @@
+function FilterDoctors(selectBox) {
+    let specialization_id = selectBox.value;
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("doctors-list").innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", "../Application/FilterDoctors.php?specialization_id=" + specialization_id, true);
+    xhttp.send();
+}
