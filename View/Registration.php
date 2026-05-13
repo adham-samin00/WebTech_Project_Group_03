29262fa845f891d983b9a3b1960e9eb42f935b2f
adham-samin00 <adhamsamin0@gmail.com>
2026-05-13 11:02:25 +0600

    Registration page issue fixed

--- View/Registration.php
+++ View/Registration.php
@@ -9,7 +9,6 @@ include "../Controller/RegistrationController.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - MediBook</title>
-    <link rel="stylesheet" href="style.css">
     <script src="../Controller/JS/CheckUserName.js"></script>
 </head>
 <body>
