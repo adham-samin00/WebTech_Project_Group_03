ee2a23c0d2cb8ce6cba1a9baf653973792704ae8
adham-samin00 <adhamsamin0@gmail.com>
2026-05-14 15:11:05 +0600

    PatientHome navigation added

--- View/PatientHome.php
+++ View/PatientHome.php
@@ -15,6 +15,11 @@ include "../Controller/ProfileController.php";
 
     <div class="topnav">
         <a href="PatientHome.php" class="sitename"><span>Medi</span>Book</a>
+    <ul class="nav-links">
+        <li><a href="BrowseDoctors.php">Find a Doctor</a></li>
+        <li><a href="MyAppointments.php">My Appointments</a></li>
+        <li><a href="PatientHome.php">Home</a></li>
+    </ul>
         <div class="nav-right">
             <p>Hello, <?php echo htmlspecialchars($_SESSION["name"]); ?></p>
             <a href="../Controller/Logout.php" class="logout-btn">Logout</a>
