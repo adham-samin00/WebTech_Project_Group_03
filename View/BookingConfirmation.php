9afc17a43ce9061615d85c81df76d952171daf6b
srijandutta1122-tech <srijandutta1122@gmail.com>
2026-05-14 22:53:27 +0600

    css connection

--- View/BookingConfirmation.php
+++ View/BookingConfirmation.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../View/CSS/BookingConfirmation.css">
       <title>Booking Confirmed - MediBook</title> 
 </head>
 <body>
