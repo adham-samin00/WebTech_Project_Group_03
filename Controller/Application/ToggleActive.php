adcd578ab969eb08b82248732f1e359661a4fed1
adham-samin00 <adhamsamin0@gmail.com>
2026-05-18 01:35:27 +0600

    Fix: Uncomment error message for update failure in ToggleActive.php

--- Controller/Application/ToggleActive.php
+++ Controller/Application/ToggleActive.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             }
         else
             {
-                //echo json_encode(["ok" => false, "message" => "Update failed"]);
+                echo json_encode(["ok" => false, "message" => "Update failed"]);
             }
     }
 else
